35096be5b43cee1dc9c2833c5a8015b88715fee7
STepan8722 <Kaigorodsev_87@mail.ru>
2023-01-27 14:10:03 +0700

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Stepan Kaygorodtsev");
+        System.out.println("22.01.1987");
+    }
+}
+
