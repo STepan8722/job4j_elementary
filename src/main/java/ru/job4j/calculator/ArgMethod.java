860151f432ecec3f3084bd13aed5889522e6a7c8
STepan8722 <Kaigorodsev_87@mail.ru>
2023-02-06 06:35:42 +0700

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -4,7 +4,8 @@ public class ArgMethod {
     public static void hello(String name, int age) {
         System.out.println("Hello," + name + ", age = " + age);
     }
-    public static void main(String[] args ) {
+
+    public static void main(String[] args) {
         String name = "job4j";
         int age = 6;
         ArgMethod.hello(name, age);
